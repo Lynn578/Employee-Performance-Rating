1dce79c2864398e021ed4614289c89c6cffcb1cc
Lynn578 <wambuievalyn87@gmail.com>
2024-08-17 22:10:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ years. INX's human resource policies are considered employee-friendly and widely
 
 # Load data
 
-df = pd.read_excel('C:\\Users\\Admin\\Desktop\\Employee-Performance-Rating\\INX_Future_Inc_Employee_Performance_CDS_Project2_Data_V1.8.xls')
+df = pd.read_excel('C:INX_Future_Inc_Employee_Performance_CDS_Project2_Data_V1.8.csv")
 st.write("Dataset loaded successfully!")
 
 
